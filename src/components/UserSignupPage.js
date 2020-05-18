db16758dab15533201958949625290ce3c090471
Oray Kurt <oray.kt@gmail.com>
2020-05-19 02:29:54 +0300

    Npm audit Fix

--- src/components/UserSignupPage.js
+++ src/components/UserSignupPage.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import axios from 'axios'
 
 class UserSignupPage extends React.Component {
   state = {
@@ -9,38 +10,76 @@ class UserSignupPage extends React.Component {
   }
 
   onChange = (event) => {
-    const {name,value} = event.target
+    const { name, value } = event.target
     this.setState({
       [name]: value,
     })
   }
 
+  onClickSignup = async (event) => {
+    try {
+      event.preventDefault()
+
+      const { userName, displayName, password } = this.state
+      const body = {
+        userName,
+        displayName,
+        password,
+      }
+      await axios.post('/api/1.0/users', body)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   render() {
     return (
-      <form>
-        <h1>Sign Up</h1>
-        <div className=''>
-          <label htmlFor=''>Username</label>
-          <input type='text' name='userName' onChange={this.onChange} />
-        </div>
-        <div className=''>
-          <label htmlFor=''>Display Name</label>
-          <input type='text' name='displayName' onChange={this.onChange} />
-        </div>
-        <div className=''>
-          <label htmlFor=''>Password</label>
-          <input type='password' name='password' onChange={this.onChange} />
-        </div>
-        <div className=''>
-          <label htmlFor=''>Password Repeat</label>
-          <input
-            type='password'
-            name='passwordRepeat'
-            onChange={this.onChange}
-          />
-        </div>
-        <button>Sign Up</button>
-      </form>
+      <div className='container'>
+        <form>
+          <h1 className='text-center'>Sign Up</h1>
+          <div className='form-group'>
+            <label htmlFor=''>Username</label>
+            <input
+              type='text'
+              name='userName'
+              className='form-control'
+              onChange={this.onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor=''>Display Name</label>
+            <input
+              type='text'
+              name='displayName'
+              className='form-control'
+              onChange={this.onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor=''>Password</label>
+            <input
+              type='password'
+              name='password'
+              className='form-control'
+              onChange={this.onChange}
+            />
+          </div>
+          <div className='form-group'>
+            <label htmlFor=''>Password Repeat</label>
+            <input
+              type='password'
+              name='passwordRepeat'
+              className='form-control'
+              onChange={this.onChange}
+            />
+          </div>
+          <div className='text-center'>
+            <button className='btn btn-primary' onClick={this.onClickSignup}>
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
     )
   }
 }
